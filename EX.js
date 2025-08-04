b66de92c2bc1ab94cc89429a310e021da32e099b
leanggek08-ui <leanggek08@gmail.com>
2025-08-04 12:01:45 +0700

    Add files via upload

--- EX.js
+++ EX.js
@@ -11,3 +11,22 @@ console.log("=== Homework: Grade Calculator ===");
 
 // TODO: Step 4 - Use if-else to print the overall grade
 // Grade rules: A (90+), B (80–89), C (70–79), F (<70)
+let array=[80,90,100,80,90];
+let sum=0;
+for(i=0;i<array.length;i++){
+    sum+=array[i];
+}
+console.log("Total score is:",sum);
+let average=sum/5;
+console.log("Average score is:",average);
+if(average>=90){
+    console.log("You get Grade A");
+}else if(average>=80){
+    console.log("You get Grade B");
+
+}else if(average>=70){
+    console.log("You get Grade C");
+
+}else{
+    console.log("You get Grade F");
+}
